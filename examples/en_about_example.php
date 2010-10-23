28afa94dd732da75d0239857d6dd77a775bd5eed
Daan Le Duc <daan@webpirates.nl>
2010-10-23 22:50:51 +0200

    Update for about page

--- examples/en_about_example.php
+++ examples/en_about_example.php
@@ -4,8 +4,8 @@
     <div id="about">
         <h2>About PHP jqGrid</h2>
         <p>All source code is licensed under the <a href="http://www.gnu.org/licenses/lgpl.html" target="_blank">GNU Lesser General Public License.</a><br />
-        Additional information can be found in <a href="" target="_blank">this</a> blogarticle.
-        <p>Source is hosted on <a href="#" target="_blank">github</a>.</p>
+        Additional information can be found in <a href="http://webpirates.nl/webpirates/daan-le-duc/50-fluent-jqgrid-helper-for-php" target="_blank">this</a> blogarticle.
+        <p>Source is hosted on <a href="http://github.com/daanl/jqGrid-Helper-PHP" target="_blank">github</a>.</p>
 
 		<p>
 		My colleague <a href="http://webpirates.nl/webpirates/robin-van-der-knaap/" target="_blank">Robin van der Knaap</a> has made jqGrid Helper for .NET MVC in #c information about jqGrid Helper in .NET #c please visit:
--- examples/nl_about_example.php
+++ examples/nl_about_example.php
@@ -4,8 +4,8 @@
     <div id="about">
         <h2>Over PHP jqGrid</h2>
         <p>Alle code is uitgegeven onder de licentie <a href="http://www.gnu.org/licenses/lgpl.html" target="_blank">GNU Lesser General Public License.</a><br />
-        Extra information kan je vinden in <a href="" target="_blank">dit</a> blogartikel.
-        <p>Source is hosted on <a href="#" target="_blank">github</a>.</p>
+        Extra information kan je vinden in <a href="http://webpirates.nl/webpirates/daan-le-duc/50-fluent-jqgrid-helper-for-php" target="_blank">dit</a> blogartikel.
+        <p>Source is hosted on <a href="http://github.com/daanl/jqGrid-Helper-PHP" target="_blank">github</a>.</p>
 
 		<p>
 		Mijn collega <a href="http://webpirates.nl/webpirates/robin-van-der-knaap/" target="_blank">Robin van der Knaap</a> heeft de jqGrid Helper voor .NET MVC in c# gemaakt. Informatie over de jqGrid Helper in .NET c# kan je vinden in
